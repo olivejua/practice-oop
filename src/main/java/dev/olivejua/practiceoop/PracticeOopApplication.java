5a7452b9dc56ccfa6e29a3924aac64806e7c7646
olivejuwa <tmfrl4710@gmail.com>
2021-05-17 21:12:06 +0900

    프로젝트 생성

---
+++ src/main/java/dev/olivejua/practiceoop/PracticeOopApplication.java
@@ -0,0 +1,13 @@
+package dev.olivejua.practiceoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeOopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeOopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/olivejua/practiceoop/PracticeOopApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.olivejua.practiceoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeOopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
