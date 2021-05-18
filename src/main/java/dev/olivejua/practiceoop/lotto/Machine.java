9143e980c3cad8432980324d552d2133af7ee646
olivejuwa <tmfrl4710@gmail.com>
2021-05-18 17:46:24 +0900

    [lotto] 기계 안의 공들을 섞었다.

--- src/main/java/dev/olivejua/practiceoop/lotto/Machine.java
+++ src/main/java/dev/olivejua/practiceoop/lotto/Machine.java
@@ -1,14 +1,20 @@
 package dev.olivejua.practiceoop.lotto;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Machine {
-    private List<Integer> balls = new ArrayList<>();
+    List<Integer> balls = new ArrayList<>();
 
-    public void putBalls() {
-        for (int i=1; i<=45; i++) {
+    public void putBalls(int sizeOfBalls) {
+        for (int i=1; i<=sizeOfBalls; i++) {
             balls.add(i);
         }
+    }
+
+    public void mixBalls() {
+        Collections.shuffle(balls);
     }
 }
--- src/test/java/dev/olivejua/practiceoop/lotto/MachineTest.java
+++ src/test/java/dev/olivejua/practiceoop/lotto/MachineTest.java
@@ -1,19 +1,70 @@
 package dev.olivejua.practiceoop.lotto;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MachineTest {
 
-    @Test
-    void getBalls() {
-        Machine machine = new Machine();
-        machine.putBalls();
-//        List<Integer> balls = machine.balls;
+    private Machine machine;
 
-//        assertEquals(45, balls.size());
+    @BeforeEach
+    void setup() {
+        machine = new Machine();
+    }
+
+    @Test
+    void putBalls() {
+        int sizeOfBalls = 45;
+        machine.putBalls(sizeOfBalls);
+        List<Integer> balls = machine.balls;
+
+        assertEquals(sizeOfBalls, balls.size());
+    }
+
+    @Test
+    void notEqualBallsBeforeMixing() {
+        int sizeOfBalls = 45;
+        machine.putBalls(sizeOfBalls);
+
+        List<Integer> expectedBalls = new ArrayList<>();
+        for (int i=1; i<= machine.balls.size(); i++) {
+            expectedBalls.add(i);
+        }
+
+        assertTrue(equalsBalls(expectedBalls, machine.balls));
+    }
+
+    @Test
+    void mixBalls() {
+        int sizeOfBalls = 45;
+        machine.putBalls(sizeOfBalls);
+        machine.mixBalls();
+
+        List<Integer> expectedBalls = new ArrayList<>();
+        for (int i=1; i<= machine.balls.size(); i++) {
+            expectedBalls.add(i);
+        }
+
+        assertFalse(equalsBalls(expectedBalls, machine.balls));
+    }
+
+    private boolean equalsBalls(List<Integer> expected, List<Integer> actual) {
+        if (expected.size() != actual.size()) {
+            return false;
+        }
+
+        for (int i=0; i<expected.size(); i++) {
+            if (!expected.get(i).equals(actual.get(i))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
